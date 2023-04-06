4878ac6fcd9b3057307d8b4362e6e54ed3ecefef
Vamsi krishna <vamcaarya31@gmail.com>
2023-04-06 12:32:33 +0530

    reverse list solution

--- src/ReverseList.java
+++ src/ReverseList.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ReverseList {
+public class ReverseList {
+
+    public ListNode reverseList(ListNode head) {
+        if(head ==null){
+            return null ;
+        }
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while(current!= null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        head = prev;
+        return head;
+    }
+
 }
+
+//tc = O(n) , sc =O(1)
